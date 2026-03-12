ac95cd8fedadd937dabc036cd94c08589de6f764
Mike Boswell <boswellmike68@gmail.com>
2026-03-12 14:49:29 -0400

    Update uiAdapter.js

--- live/uiAdapter.js
+++ live/uiAdapter.js
@@ -1,1 +1,8 @@
+// live/uiAdapter.js
 
+import { adapter } from "../bb-interface/adapter.js";
+
+// This is the function your UI or runtime will call
+export function uiAdapter(rawInput, tone = "direct") {
+  return adapter(rawInput, tone);
+}
